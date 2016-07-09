9ca1667619b2840f8527cecb6c95ab6c97fe498f
giorgiSh <giorgishesheli@gmail.com>
2016-07-09 20:06:18 +0400

    added SIGWINCH handler & minor changes

--- typetype.c
+++ typetype.c
@@ -1,3 +1,4 @@
+#define _DEFAULT_SOURCE
 #include <curses.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,20 +6,29 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <ctype.h>
+#include <signal.h>
 
-#define VISIBLE_TEXT_SIZE 9000
 
 //global variables
 int cur_position = 0;
 int mistakes = 0;
 int words = 0;
 WINDOW *bot, *top;
+void (*old_handler)(int);
+int buf_count;
+char *buf;
+char *buf_opt;
 
-void handle_top(char *buf, char *buf_opt){
-	int cur_x, cur_y, i, COLOR_;
+
+
+
+
+void draw_top(){
+	int cur_x, cur_y, i;
+	int COLOR_ = 0;
 	werase(top);
-	wmove(top, 1, 0);
-	for(i = 0; i < strlen(buf); i++){
+	wmove(top, 0, 0);
+	for(i = 0; i < buf_count; i++){
 		if((int) buf_opt[i]  == 1)
 			COLOR_ = COLOR_PAIR(1);
 		if(i == cur_position)
@@ -30,7 +40,7 @@ void handle_top(char *buf, char *buf_opt){
 	wrefresh(top);
 }
 
-void handle_bot(char ch, char *buf){
+void handle_bot(char ch){
 	if(buf[cur_position] != ch)
 		mistakes += 1;
 	if(buf[cur_position] == ch && isblank(ch)){
@@ -52,13 +62,25 @@ void handle_bot(char ch, char *buf){
 void main_loop(char *buf, char *buf_opt){
 	for(;;){
 		chtype ch = wgetch(top);
-		handle_bot(ch, buf);
+		handle_bot(ch);
 		if(buf[cur_position] == ch){
 			buf_opt[cur_position] = 1;
 			cur_position++;
-			handle_top(buf, buf_opt);
+			draw_top();
+		} else {
+			wchgat(top, 1, 0, 2, NULL);
 		}
 	}
+
+}
+
+void resize_handler(int a){
+	old_handler(a);
+	//int y, x;
+	//getmaxyx(top, y, x);
+	//if(y * x < buf_count)
+		//buf_count = y*x;
+	draw_top();
 
 }
 
@@ -69,21 +91,6 @@ int main(int argc, char **argv){
 		exit(EXIT_FAILURE);
 	}
 
-
-	char buf[VISIBLE_TEXT_SIZE];
-	char buf_opt[VISIBLE_TEXT_SIZE] = {0};
-
-	FILE *file_src;
-	if((file_src = fopen(argv[1], "r")) == NULL){
-		perror("fopen");
-		exit(EXIT_FAILURE);
-	}
-	if(fread(buf, VISIBLE_TEXT_SIZE - 1, 1, file_src) == 0){
-		printf("end of file or read error, dunno");
-	}
-	buf[VISIBLE_TEXT_SIZE] = '\0';
-
-
 	//ncurses init
 	initscr();
 	start_color();
@@ -91,29 +98,42 @@ int main(int argc, char **argv){
 	noecho();
 	keypad(stdscr, TRUE);
 
-
 	//init color pairs
 	use_default_colors();
 	init_pair(1, COLOR_GREEN, -1);
+	init_pair(2, COLOR_RED, COLOR_BLACK);
 
-
+	//init windows
 	top = newwin(LINES - LINES / 3 - 1, COLS - 2, 1, 1);
-	//box(top, 0, 0);
 	wrefresh(top);
 	bot = newwin(LINES / 3, COLS - 2, LINES - LINES/3, 1);
-	//box(bot, 0, 0);
 	wrefresh(bot);
 
+	//initiate SIGWINCH handler
+	old_handler = signal(SIGWINCH, resize_handler); //todo - cfe
+
+	//read to buffer
+	int x, y;
+	getmaxyx(top, y, x);
+	buf_count = y * x;
+
+	buf = malloc(buf_count);
+	buf_opt = malloc(buf_count);
+
+
+	FILE *file_src;
+	if((file_src = fopen(argv[1], "r")) == NULL){
+		perror("fopen");
+		exit(EXIT_FAILURE);
+	}
+	if(fread(buf, buf_count  - 1, 1, file_src) == 0){
+		printf("end of file or read error, dunno");
+	}
 
 	// Initial text
-	int i;
-	wmove(top, 1, 1);
-	for(i = 0; i < strlen(buf); i++){
-		waddch(top, buf[i]);
-	}
-	wrefresh(top);
-	wmove(top, 1, 1);
+	draw_top();
 
+	//Init bottom
 	mvwaddstr(bot, 1, 1, "Key: ");
 	mvwaddstr(bot, 2, 1, "Mistakes: ");
 	mvwaddstr(bot, 3, 1, "Words: ");
